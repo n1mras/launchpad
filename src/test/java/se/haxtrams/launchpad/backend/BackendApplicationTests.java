e741156239687dd672f1ac75f92f55503b0e8eb5
karam chauhan <karam.chauhan86@gmail.com>
2022-03-06 18:02:42 +0100

    fixed tests

--- src/test/java/se/haxtrams/launchpad/backend/BackendApplicationTests.java
+++ src/test/java/se/haxtrams/launchpad/backend/BackendApplicationTests.java
@@ -2,8 +2,10 @@ package se.haxtrams.launchpad.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
 	@Test
